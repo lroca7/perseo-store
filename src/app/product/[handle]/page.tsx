cb14c64ffb1a505f988835e790ad4360735ed102
lroca7 <lizethcabrales@gmail.com>
2024-11-17 12:15:34 -0500

    Formulario de Signup

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -8,11 +8,28 @@ interface ProductPageProps {
   };
 }
 
+export async function generateMetadata({ searchParams }: ProductPageProps) {
+  const parameters = await searchParams;
+  const id = parameters.id;
+  const products = await getProducts(id);
+  const product = products[0];
+
+  return {
+    title: product.title,
+    description: product.description,
+    keywords: product.tags,
+    openGraph: {
+      images: [product.image],
+    },
+  };
+}
+
 export default async function ProductPage({ searchParams }: ProductPageProps) {
   const id = searchParams.id;
   const products = await getProducts(id);
   const product = products[0];
 
+  console.log(product);
   if (!id) {
     redirect("/store");
   }
